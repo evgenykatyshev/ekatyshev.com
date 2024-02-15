833d8b3e7616165c72b3bd5c646856f1e51075a0
evgenykatyshev <eugkat@gmail.com>
2024-02-15 16:59:14 +0300

    Update

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+<title>Evgeny Katyshev</title>
+
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,400;0,800;0,900;1,400;1,800;1,900&display=swap" rel="stylesheet">
+
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="robots" content="index, follow" />
+<meta name="viewport" content="width=device-width" />
+
+<meta name="description" content="UX designer Evgeny Katyshev" />
+<meta name="author" content="Evgeny Katyshev" />
+
+<link rel="stylesheet" type="text/css" href="/s.css" />
+
+<script src="/js/jquery-2.1.4.min.js"></script>
+<script src="/js/core.js"></script>
+
+</head>
+
+<body>
+
+<section>
+  <h1>Evgeny Katyshev</h1>
+  <h2>Product designer</h2>
+</section>
+
+<section class="social-links">
+  <a href="//instagram.com/evgenykatyshev" title="instagram">Instagram</a>
+  <a href="//twitter.com/ekatyshev" title="twitter">Twitter</a>
+  <a href="//linkedin.com/in/evgenykatyshev/" title="linkedin">Linkedin</a>
+</section>
+
+</body>
+</html>
